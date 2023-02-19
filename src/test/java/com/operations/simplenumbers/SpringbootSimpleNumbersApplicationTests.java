798d1929d970350cbccbc9a1f8ddf471356c3a92
Suleman <78090408+Sulemanshaik013@users.noreply.github.com>
2023-02-19 11:58:24 +0530

    intial commit

---
+++ src/test/java/com/operations/simplenumbers/SpringbootSimpleNumbersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.operations.simplenumbers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSimpleNumbersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
